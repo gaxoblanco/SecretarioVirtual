ab17c0aa7fd5377a44ef9a5370f3cf4328a0ff85
gaxoblanco <94192138+gaxoblanco@users.noreply.github.com>
2022-06-15 20:01:11 -0300

    Voy a conectar el proyecto a un servidor de firebase, para poder chequearlo mejor en diferentes dispositivos.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
 export const environment = {
   production: true
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyAtr-qtlhzf4eThM2RPFOfj9O3alt6gTNI",
+
+    authDomain: "secretariovirtual-34292.firebaseapp.com",
+
+    projectId: "secretariovirtual-34292",
+
+    storageBucket: "secretariovirtual-34292.appspot.com",
+
+    messagingSenderId: "765554914700",
+
+    appId: "1:765554914700:web:8bdc04df420d7c729b92a9"
+
+  };
+
 };
