8cd4e0f4cb8f87fb93b45a2261c2529f6c2a702a
Gaston <gaston@tdil.com.ar>
2023-10-23 17:00:55 -0300

    styles

--- src/app/screen/add-file-screen/add-file-screen.component.ts
+++ src/app/screen/add-file-screen/add-file-screen.component.ts
@@ -29,49 +29,62 @@ export class AddFileScreenComponent implements OnInit {
   addFile() {
     // tomo el valor fileNumber del formulario y hago split para separar los numeros
     let fileNumber = this.files.value.fileNumber;
-    // consulto si el filNumber tiene / para separar los numeros
-    if (fileNumber.includes('/')) {
-      // separo los numeros en un array
-      let fileNumberSplit = fileNumber.split('/');
-      // creo un objeto de tipo NewFile para enviarlo al servicio
-      let newFile: NewFile = {
-        fileNumber: Number(fileNumberSplit[0]),
-        yearNumber: Number(fileNumberSplit[1]),
-      };
-      // envio el objeto al servicio
-      this.fileSer.addFiles(newFile).subscribe({
-        next: () => {
-          this.status = 'success';
-          this.newFileList.push({
-            id_exp: 0,
-            numero_exp: newFile.fileNumber,
-            anio_exp: newFile.yearNumber.toString(),
-            caratula: '',
-            dependencia: '',
-          });
-        },
-        error: (error) => {
-          this.status = 'failed';
-          console.log('er1', error);
-        },
-      });
-    } else {
-      // creo un objeto de tipo NewFile separando los ultimos 2 numeros del fileNumber asignandolos al yearNumber
-      let newFile: NewFile = {
-        fileNumber: Number(fileNumber.slice(0, -2)),
-        yearNumber: Number(fileNumber.slice(-2)),
-      };
-      // envio el objeto al servicio
-      this.fileSer.addFiles(newFile).subscribe({
-        next: () => {
-          this.status = 'success';
-        },
-        error: (error) => {
-          this.status = 'failed';
-          console.log(error);
-          console.log('er2', error);
-        },
-      });
+
+    // valido que el campo no este vacio o sea == 0
+    if (fileNumber.length >= 4) {
+      // consulto si el filNumber tiene / para separar los numeros
+      if (fileNumber.includes('/')) {
+        // separo los numeros en un array
+        let fileNumberSplit = fileNumber.split('/');
+        // creo un objeto de tipo NewFile para enviarlo al servicio
+        let newFile: NewFile = {
+          fileNumber: Number(fileNumberSplit[0]),
+          yearNumber: Number(fileNumberSplit[1]),
+        };
+        // envio el objeto al servicio
+        this.fileSer.addFiles(newFile).subscribe({
+          next: () => {
+            this.status = 'success';
+            this.newFileList.push({
+              id_exp: this.newFileList.length,
+              numero_exp: newFile.fileNumber,
+              anio_exp: newFile.yearNumber.toString(),
+              caratula: '',
+              dependencia: '',
+            });
+          },
+          error: (error) => {
+            this.status = 'failed';
+            console.log('er1', error);
+          },
+        });
+      } else {
+        // creo un objeto de tipo NewFile separando los ultimos 2 numeros del fileNumber asignandolos al yearNumber
+        let newFile: NewFile = {
+          fileNumber: Number(fileNumber.slice(0, -2)),
+          yearNumber: Number(fileNumber.slice(-2)),
+        };
+        // envio el objeto al servicio
+        this.fileSer.addFiles(newFile).subscribe({
+          next: () => {
+            this.status = 'success';
+            this.newFileList.push({
+              id_exp: 0,
+              numero_exp: newFile.fileNumber,
+              anio_exp: newFile.yearNumber.toString(),
+              caratula: '',
+              dependencia: '',
+            });
+          },
+          error: (error) => {
+            this.status = 'failed';
+            console.log(error);
+            console.log('er2', error);
+          },
+        });
+      }
+    } else if (fileNumber.length != 0) {
+      this.status = 'failed';
     }
   }
 
