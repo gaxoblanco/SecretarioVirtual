e3bdb848783a2ab159972210857cc95482453500
Gaston <gaston@tdil.com.ar>
2023-12-03 10:27:29 -0300

    up - delete_dispatch - try por cada consulta la base de datos

--- API-sv/dispatch/delete_dispatch.php
+++ API-sv/dispatch/delete_dispatch.php
@@ -25,7 +25,12 @@ class delete_dispatch
         echo "Error: El usuario no existe.";
         return;
       }
-
+    } catch (PDOException $e) {
+      $response = [
+        'error' => 'Error de conexión: ' . $e->getMessage()
+      ];
+    }
+    try {
       // Verificar si el expediente existe para el usuario dado
       $query = $this->conexion->prepare('SELECT * FROM user_expedients WHERE id_user = :id_user AND CONCAT(id_exp) = :id_exp');
       $query->execute([':id_user' => $this->userId, ':id_exp' => $this->dispatchId]);
@@ -35,18 +40,39 @@ class delete_dispatch
         echo "Error: No se encontró el expediente.";
         return;
       }
-
+    } catch (PDOException $e) {
+      $response = [
+        'error' => 'Error de conexión: ' . $e->getMessage()
+      ];
+    }
+    try {
       // Consulta en la tabla user_exp_move si existe algun movimiento para el expediente
       $query = $this->conexion->prepare('SELECT * FROM user_exp_move WHERE id_exp = :id_exp');
       $query->execute([':id_exp' => $this->dispatchId]);
       $dispatchHasMoves = $query->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+      $response = [
+        'error' => 'Error de conexión: ' . $e->getMessage()
+      ];
+      echo json_encode($response);
+      return;
+    }
 
+    try {
       // Si el expediente tiene movimientos, los elimina
       if ($dispatchHasMoves) {
         $query = $this->conexion->prepare('DELETE FROM user_exp_move WHERE id_exp = :id_exp');
         $query->execute([':id_exp' => $this->dispatchId]);
       }
+    } catch (PDOException $e) {
+      $response = [
+        'error' => 'Error de conexión: ' . $e->getMessage()
+      ];
+      echo json_encode($response);
+      return;
+    }
 
+    try {
       // Eliminar el expediente de la tabla user_expedients
       $query = $this->conexion->prepare('DELETE FROM user_expedients WHERE id_exp = :id_exp AND CONCAT(id_exp) = :id_exp');
       $query->execute([':id_user' => $this->userId, ':id_exp' => $this->dispatchId]);
