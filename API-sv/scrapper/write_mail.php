191fe4f089a8f31facb69d2a8d69d19583195f56
Gaston <gaston@tdil.com.ar>
2024-03-30 18:13:41 +0100

    clear files

--- API-sv/scrapper/write_mail.php
+++ API-sv/scrapper/write_mail.php
@@ -139,56 +139,68 @@ class write_mail
   // 3 - Create the message of the email
   private function createMessage($news)
   {
-    $message = "<html><body>";
+    $message = "<html><body class='color=black;>";
     $message .= "<h1>Notificación de expedientes</h1>";
     $message .= "<h2>Estimado/a " . $news['name'] . "</h2>";
     $message .= "<p>Se le notifica que los siguientes expedientes tuvieron movimientos:</p>";
 
     foreach ($news['expedients'] as $expedient) {
+      // convierto valor numero de $expedient['dependencia'] a su version en texto con textDependenci
+      // /dependencia_mixing.php se encuentra en el directorio principal del proyecto
+      require_once __DIR__ . '/../dependencia_mixing.php';
+      // valido que textDependencia se pudo importar correctamente
+      if (function_exists('textDependencia')) {
+        $expedient['dependencia'] = textDependencia($expedient['dependencia']);
+      }
+
+      // linea azul para separar los expedientes
       $message .= "<div style='height: 4px; background-color: #37bbed; margin: 30px; border-radius: 10px;'>";
       $message .= "</div>";
-      $message .= "<div style='max-width: 80%; border: 1px solid #a0bdcf; padding: 10px; margin: 0 auto; border-top: 20px solid #37bbed; border-radius: 12px;'>";
+      // datos del expediente
+      $message .= "<div style='color:black; max-width: 80%; border: 1px solid #a0bdcf; padding: 10px; margin: 0 auto; border-top: 20px solid #37bbed; border-radius: 12px;'>";
       $message .= "<h3>Expediente: " . $expedient['numero_exp'] . "/" . $expedient['anio_exp'] . "</h3>";
       $message .= "<div style='padding: 10px; margin: 10px;'>";
       $message .= "<h4>Carátula: " . $expedient['caratula'] . "</h4>";
       $message .= "<h4>Reservado: " . $expedient['reservado'] . "</h4>";
-      $message .= "<h4>Dependencia: " . $expedient['dependencia'] . "</h4>";
+      // $message .= "<h4>Dependencia: " . $expedient['dependencia'] . "</h4>";
+      // agrego dependencia y el valor lo proceso para usar caracteres especiales
+      $message .= "<h4>Dependencia: " . htmlentities($expedient['dependencia'], ENT_QUOTES, 'UTF-8') . "</h4>";
       $message .= "<h4>Tipo de lista: " . $expedient['tipo_lista'] . "</h4>";
       $message .= "</div>";
-      $message .= "<h4 style='font-size: medium;'>Movimientos:</h4>";
+      $message .= "<h4 style='font-size: medium;'>Movimiento:</h4>";
 
 
       //si $expedient['movimientos'] escribo el expediente aun no tiene movimientos
-      if (!isset($expedient['movimientos']) || empty($expedient['movimientos'])) { //si $expedient['movimientos'] esta vacio
-        $message .= "<div style='border: 1px solid #a0bdcf; padding: 10px; margin: 10px; border-top: 10px solid #37bbed;'>";
+      if (!empty($expData['movimientos'])) { //si $expedient['movimientos'] esta vacio
+        $message .= "<div style='border: 1px solid #a0bdcf; padding: 10px; margin: 10px; border-top: 10px solid #37bbed; border-radius: 12px;'>";
         $message .= "<h5 style='font-weight: bold; font-size:14px;'>El expediente aun no tiene movimientos</h5>";
         $message .= "</div>";
+      } else {
+
+        foreach ($expedient['movimientos'] as $movimiento) {
+          $message .= "<div style='border: 1px solid #a0bdcf; margin: 10px; border-radius: 12px;'>";
+          $message .= "<div style='border-bottom: 1px solid #a0bdcf; padding: 0 10px;'>";
+          $message .= "<h5 style='font-weight: bold; font-size:14px; display: flex;'>Fecha de movimiento: " .
+            "<p style='margin:0; margin-left: 10px;'>" . $movimiento['fecha_movimiento'] . "</p>" . "</h5>";
+          $message .= "<h5 style='font-weight: bold; font-size:14px; display: flex;'>Estado: " .
+            "<p style='margin:0; margin-left: 10px;'>" . $movimiento['estado'] . "</p>" . "</h5>";
+          $message .= "</div>";
+
+          $message .= "<h5 style='margin: 10px; font-weight: bold; font-size:14px;'>Texto: " .
+            "<p style='padding: 0px 10px;'>" . $movimiento['texto'] . "</p>" . "</h5>";
+          $message .= "<h5 style='margin: 10px; font-weight: bold; font-size:14px;'>Título: " .
+            "<p style='padding: 0px 10px;'>" . $movimiento['titulo'] . "</p>" . "</h5>";
+          // valido que $movimiento['despacho'] no este vacio
+          if (!empty($movimiento['despacho'])) {
+            $message .= "<h5 style='margin: 10px; font-weight: bold; font-size:14px;'>Despacho: " .
+              "<p style='padding: 0px 10px;'>" . $movimiento['despacho'] . "</p>" . "</h5>";
+          }
+          $message .= "</div>";
+        }
       }
-
-      // Modificar para solo trabajar con el ULTIMO movimiento del exp.
-      foreach ($expedient['movimientos'] as $movimiento) {
-        $message .= "<div style='border: 1px solid #a0bdcf; margin: 10px; border-radius: 12px;'>";
-        $message .= "<div style='border-bottom: 1px solid #a0bdcf; padding: 0 10px;'>";
-        $message .= "<h5 style='font-weight: bold; font-size:14px; display: flex;'>Fecha de movimiento: " .
-          "<p style='margin:0; margin-left: 10px;'>" . $movimiento['fecha_movimiento'] . "</p>" . "</h5>";
-        $message .= "<h5 style='font-weight: bold; font-size:14px; display: flex;'>Estado: " .
-          "<p style='margin:0; margin-left: 10px;'>" . $movimiento['estado'] . "</p>" . "</h5>";
-        $message .= "</div>";
-
-        $message .= "<h5 style='margin: 10px; font-weight: bold; font-size:14px;'>Texto: " .
-          "<p style='margin: 8px; margin-left: 12px;'>" . $movimiento['texto'] . "</p>" . "</h5>";
-        $message .= "<h5 style='margin: 10px; font-weight: bold; font-size:14px;'>Título: " .
-          "<p style='margin: 8px; margin-left: 12px;>" . $movimiento['titulo'] . "</p>" . "</h5>";
-        $message .= "<h5 style='margin: 10px; font-weight: bold; font-size:14px;'>Despacho: " .
-          "<p style='margin: 8px; margin-left: 12px;>" . $movimiento['despacho'] . "</p>" . "</h5>";
-        $message .= "</div>";
-      }
-
       $message .= "</div>";
     }
     $message .= "</body></html>";
-
-    echo "body creado";
 
     return $message;
   }
